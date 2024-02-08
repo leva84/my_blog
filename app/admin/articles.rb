10cf9c42257189e959526e4786b025050d866a9f
Denis Levchenko <denfrut8@gmail.com>
2024-02-08 09:08:00 +0300

    Added gem for text editor

--- app/admin/articles.rb
+++ app/admin/articles.rb
@@ -1,3 +1,13 @@
 ActiveAdmin.register Article do
   permit_params :title, :summary, :content
+
+  form do |f|
+    f.inputs do
+      f.input :title
+      f.input :summary
+      f.input :content, as: :simplemde_editor
+    end
+
+    f.actions
+  end
 end
